d9f0445335fa4cc150e7ea78c7a95c697a49db4e
Kirill Lopatin <lopatin.kg@gmail.com>
2023-12-20 21:12:46 +0300

    fixes: ruff toml

--- run_pooling.py
+++ run_pooling.py
@@ -1,9 +1,11 @@
-import os, django
+import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dtb.settings")
 django.setup()
 
-from tgbot.dispatcher import run_pooling
+from tgbot.dispatcher import run_pooling  # noqa
 
 if __name__ == "__main__":
     run_pooling()
